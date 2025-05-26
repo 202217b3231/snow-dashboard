a4abba855cc6233e306b4560b31e3b9e4412bfb8
Himanshu <manshu3220@gmail.com>
2025-05-26 12:02:37 +0530

    intial

--- src/routes/Home.jsx
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-  return (
-    <>
-      <h2 className="">Home Page</h2>
-    </>
-  );
-};
-
-export default Home;
--- src/routes/Settings.jsx
+++
@@ -1,9 +0,0 @@
-const Setting = () => {
-  return (
-    <>
-      <h2>Setting Page</h2>
-    </>
-  );
-};
-
-export default Setting;
