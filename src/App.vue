635bbdc649db5ad663fe85450881d452bccc2ae4
Himanshu <manshu3220@gmail.com>
2025-05-26 13:00:47 +0530

    added strcuture

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,8 @@
+<script setup></script>
+
 <template>
-    <h1>Hello world
+    <aside></aside>
+    <main></main>
 </template>
 
-<style scoped>
-.logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-}
-.logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style></style>
