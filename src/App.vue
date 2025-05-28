8ce701a5ec83d09d05c1324aaa3405891edaf93a
202217b3231 <manshu3220@gmail.com>
2025-05-28 07:12:44 -0400

    added theme

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,71 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted } from "vue";
+import {
+  Home as HomeIcon,
+  LayoutDashboard as DashboardIcon,
+  NotebookTabs as NotesIcon,
+  Settings as SettingsIcon,
+  SunDim,
+  Moon,
+} from "lucide-vue-next";
+
+const routes = [
+  {
+    url: "/",
+    name: "Home",
+    icon: HomeIcon,
+  },
+  {
+    url: "/dashboard",
+    name: "Dashboard",
+    icon: DashboardIcon,
+  },
+  {
+    url: "/notes",
+    name: "Notes",
+    icon: NotesIcon,
+  },
+  {
+    url: "/settings",
+    name: "Settings",
+    icon: SettingsIcon,
+  },
+];
+const currentTheme = ref("pastel");
+const setTheme = () => {
+  const newtheme = currentTheme.value === "dim" ? "pastel" : "dim";
+  if (typeof document != "undefined") {
+    document.documentElement.setAttribute("data-theme", newtheme);
+    localStorage.setItem("theme", newtheme);
+  }
+  currentTheme.value = newtheme;
+};
+onMounted(() => {
+  if (typeof window != undefined) {
+    const getTheme = localStorage.getItem("theme");
+    if (getTheme === "pastel" || getTheme === "dim") {
+      currentTheme.value = getTheme;
+      document.documentElement.setAttribute("data-theme", getTheme);
+    }
+  }
+});
+</script>
 
 <template>
   <div class="flex h-screen w-screen">
-    <aside class="bg-red-200 w-1/6">
+    <aside class="w-1/6">
       <ul class="flex flex-col gap-4 p-4">
-        <router-link to="/">Go to Home</router-link>
-        <router-link to="/dashboard">Dashboard</router-link>
-        <router-link to="/notes">Notes</router-link>
+        <router-link v-for="route in routes" :key="route.name" :to="route.url">
+          <component :is="route.icon" class="w-5 h-5" />
+        </router-link>
       </ul>
+      <p v-on:click="setTheme" class="cursor-pointer">
+        <SunDim v-if="currentTheme === 'dim'" />
+        <Moon v-else-if="currentTheme === 'pastel'" />
+      </p>
     </aside>
-    <main class="bg-blue-200 flex-1"><router-view></router-view></main>
+    <main class="flex-1">
+      <router-view></router-view>
+    </main>
   </div>
 </template>
-
-<style></style>
--- src/main.js
+++ src/main.js
@@ -1,24 +1,25 @@
+import { createWebHashHistory, createRouter } from "vue-router";
 import { createApp } from "vue";
+
 import "./style.css";
 import App from "./App.vue";
-
-import { createMemoryHistory, createRouter } from "vue-router";
-import Home from "./views/Home.vue";
-import Dashboard from "./views/Dashboard.vue";
-import Data from "./views/Data.vue";
-import Notes from "./views/Notes.vue";
-import Settings from "./views/Settings.vue";
+import Home from "@/views/Home.vue";
+import Dashboard from "@/views/Dashboard.vue";
+import Data from "@/views/Data.vue";
+import Notes from "@/views/Notes.vue";
+import Settings from "@/views/Settings.vue";
 
 const routes = [
-  { path: "/", component: Home },
-  { path: "/dashboard", component: Dashboard },
-  { path: "/data", component: Data },
-  { path: "/notes", component: Notes },
-  { path: "/settings", component: Settings },
+  { path: "/", name: "home", component: Home },
+  { path: "/dashboard", name: "dashboard", component: Dashboard },
+  { path: "/data", name: "data", component: Data },
+  { path: "/notes", name: "notes", component: Notes },
+  { path: "/settings", name: "settings", component: Settings },
 ];
 
 const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHashHistory(),
   routes,
 });
+
 createApp(App).use(router).mount("#app");
